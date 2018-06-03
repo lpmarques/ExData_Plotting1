9cd8eff8ce255003ebff084b13f74d08a93f5646
Lucas Marques <lucasmarques.bio@gmail.com>
2018-06-03 20:30:16 -0300

    explanatory comments

--- hpc_analysis.R
+++ hpc_analysis.R
@@ -11,10 +11,7 @@ hist(hpc$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ma
 dev.print(png,"plot1.png",width=480); # prints plot to png
 
 # plot 2
-with(hpc,{
-	plot(dateTime,Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="n");
-	lines(dateTime,Global_active_power);
-})
+plot(hpc$dateTime,hpc$Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="l");
 dev.print(png,"plot2.png",width=480); # prints plot to png
 
 # plot 3
@@ -23,15 +20,14 @@ with(hpc,{
 	lines(dateTime,Sub_metering_1);
 	lines(dateTime,Sub_metering_2,col="red");
 	lines(dateTime,Sub_metering_3,col="blue");
-	legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),col=c("black","red","blue"));
+	legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"));
 })
 dev.print(png,"plot3.png",width=480); # prints plot to png
 
 # plot 4
 par(mfcol=c(2,2));
 with(hpc,{
-	plot(dateTime,Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="n");
-	lines(dateTime,Global_active_power);
+	plot(dateTime,Global_active_power,ylab="Global Active Power (kilowatts)",xlab="",type="l");
 
 	plot(dateTime,Sub_metering_1,ylab="Energy sub metering",xlab="",type="n");
 	lines(dateTime,Sub_metering_1);
@@ -39,10 +35,8 @@ with(hpc,{
 	lines(dateTime,Sub_metering_3,col="blue");
 	legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1),col=c("black","red","blue"),box.lty=0);
 
-	plot(dateTime,Voltage,xlab="",type="n");
-	lines(dateTime,Voltage);
+	plot(dateTime,Voltage,xlab="",type="l");
 
-	plot(dateTime,Global_reactive_power,ylab="Global Reactive Power (kilowatts)",xlab="",type="n");
-	lines(dateTime,Global_reactive_power);
+	plot(dateTime,Global_reactive_power,ylab="Global Reactive Power (kilowatts)",xlab="",type="l");
 })
 dev.print(png,"plot4.png",width=480) # prints plot to png
